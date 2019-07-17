4b4dbd39be0ee4795025546a792c1a77973d4bdb
Purujit Bahl <purujit.bahl@ge.com>
2019-07-17 14:55:14 -0700

    added a landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'getting-started', component: NavbarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
