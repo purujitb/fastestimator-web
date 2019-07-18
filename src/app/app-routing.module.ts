9ea7d2f07a04757a43c7b33d1a7bfb8079f9ea4f
Purujit Bahl <purujit.bahl@ge.com>
2019-07-17 17:02:07 -0700

    added tutorial section in navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GettingStartedComponent } from './getting-started/getting-started.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
 
 
 const routes: Routes = [
   { path: 'getting-started', component: GettingStartedComponent },
+  { path: 'tutorial', component: TutorialComponent },
 ];
 
 @NgModule({
